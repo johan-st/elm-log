13a9e746ee2c0572eab91d1a2ab59e7629d15350
johan-STYL <53336235+johan-STYL@users.noreply.github.com>
2020-12-07 11:48:57 +0100

    reboot

--- README.md
+++
@@ -1,7 +0,0 @@
-# Elm-Log
-
-web-ui for my logs.
-
-### BUGS
-
-- UI: latest log is always dated to when request is sent
--- server/database/Schemas/log.js
+++
@@ -1,7 +0,0 @@
-const { Schema } = require('mongoose');
-
-module.exports = new Schema({
-  logType: String,
-  time: { type: Date, default: Date.now },
-  data: {},
-});
